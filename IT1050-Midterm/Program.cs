6da8eaa81af8610d352200c39e8321eb873dec97
IT1050-AaronOsowski <aaron.osowski@yahoo.com>
2017-10-25 19:43:04 -0400

    Add project files.

---
+++ IT1050-Midterm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IT1050_Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
